489bd74dc84c6b34594f5d3816b651bbee7c1a0e
Vinay Krishna <vinaykrishnakolar@gmail.com>
2024-09-18 22:41:03 +0530

    2 files added

--- src/test/java/org/example/Test002.java
+++ src/test/java/org/example/Test002.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import io.restassured.RestAssured;
+
 public class Test002 {
+    public static void main(String[] args) {
+        System.out.println("REST Assured test case");
+        System.out.println("GET request demo");
+
+        // Gherkin syntax
+        // Given() - url, header, payload or Body
+        // When() - http methods - GET, POST, PATCH, PUT, DELETE
+        // Then() - Verify the response, Expected result = Actual Result
+
+
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/1").log().all()
+                .when()
+                    .get()
+                .then().log().all()
+                    .statusCode(200); // Output will throw error when 200 is changed
+
+    }
 }
