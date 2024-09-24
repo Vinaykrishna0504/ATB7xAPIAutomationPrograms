5fe1be501cafab051a8d3316cc3bb01c4a245a9e
Vinay Krishna <vinaykrishnakolar@gmail.com>
2024-09-24 09:31:05 +0530

    2 files added

--- src/test/java/API/Testing/ex_15092024/CRUD/PUT/NonBDDPUT.java
+++ src/test/java/API/Testing/ex_15092024/CRUD/PUT/NonBDDPUT.java
@@ -7,28 +7,29 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
-    public class NonBDDPUT{
-        RequestSpecification requestSpecification;
-        ValidatableResponse validatableResponse;
-        Response response;
-        // 6118
-        // Token -  Token?
-        // Payload - {
-        //    "firstname" : "Pramod",
-        //    "lastname" : "Brown",
-        //    "totalprice" : 111,
-        //    "depositpaid" : true,
-        //    "bookingdates" : {
-        //        "checkin" : "2018-01-01",
-        //        "checkout" : "2019-01-01"
-        //    },
-        //    "additionalneeds" : "Breakfast"
-        //}
+public class NonBDDPUT{
+
+    RequestSpecification requestSpecification;
+    ValidatableResponse validatableResponse;
+    Response response;
+    // 6118
+    // Token -  Token?
+    // Payload - {
+    //    "firstname" : "Pramod",
+    //    "lastname" : "Brown",
+    //    "totalprice" : 111,
+    //    "depositpaid" : true,
+    //    "bookingdates" : {
+    //        "checkin" : "2018-01-01",
+    //        "checkout" : "2019-01-01"
+    //    },
+    //    "additionalneeds" : "Breakfast"
+    //}
 
 
-        // POST - Auth - token
-        // POST - Booking ID
-        // PUT - token and BookingID
+    // POST - Auth - token
+    // POST - Booking ID
+    // PUT - token and BookingID
 
 
 //    public void get_token(){
@@ -39,40 +40,54 @@ import org.testng.annotations.Test;
 //        return bookingid;
 //    }
 
-        @Test
-        public void test_put_positive_tc(){
-            // POST - Auth - token
+    @Test
+    public void test_put_positive_tc(){
+        // POST - Auth - token
 
-            String token  = "0d5c10c0a1064ce";
-            String bookingid = "3159";
+        String token  = "0d5c10c0a1064ce";
+        String bookingid = "3159";
 
-            String payloadPUT= "{\n" +
-                    "    \"firstname\" : \"Pramod\",\n" +
-                    "    \"lastname\" : \"Dutta\",\n" +
-                    "    \"totalprice\" : 111,\n" +
-                    "    \"depositpaid\" : false,\n" +
-                    "    \"bookingdates\" : {\n" +
-                    "        \"checkin\" : \"2024-01-01\",\n" +
-                    "        \"checkout\" : \"2024-01-01\"\n" +
-                    "    },\n" +
-                    "    \"additionalneeds\" : \"Lunch\"\n" +
-                    "}";
-
-
-            requestSpecification = RestAssured.given();
-            requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
-            requestSpecification.basePath("/booking/"+bookingid);
-            requestSpecification.contentType(ContentType.JSON);
-            requestSpecification.cookie("token",token);
-            requestSpecification.body(payloadPUT).log().all();
-
-            Response response = requestSpecification.when().put();
+        String payloadPUT= "{\n" +
+                "    \"firstname\" : \"Pramod\",\n" +
+                "    \"lastname\" : \"Dutta\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : false,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2024-01-01\",\n" +
+                "        \"checkout\" : \"2024-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Lunch\"\n" +
+                "}";
 
 
-            // Get Validatable response to perform validation
-            validatableResponse = response.then().log().all();
-            validatableResponse.statusCode(200);
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com/");
+        requestSpecification.basePath("/booking/"+bookingid);
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.cookie("token",token);
+        requestSpecification.body(payloadPUT).log().all();
 
-        }
+        Response response = requestSpecification.when().put();
+
+
+        // Get Validatable response to perform validation
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
+
+
+
+}
